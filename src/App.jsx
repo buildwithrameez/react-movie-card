31d93ed0543f50b38d4967d97f83dc3ad43ce60f
Muhammad Rameez <buildwithrameez@gmail.com>
2026-08-06 23:55:36 +0500

    remove parent element and use array or fragment in react use remove extra parent element

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 
 export const Moviecard  = () => {
-  return <div>
-    <NetflixSeries />
-    <NetflixSeries />
-    <NetflixSeries />
-    <NetflixSeries />
-    <NetflixSeries />
+  return <>
+  <NetflixSeries />
+  <NetflixSeries />
+  <NetflixSeries />
+  <NetflixSeries />
+  <NetflixSeries />
+  </>
+   
+// in react it return only one elements so we add one parent and its childeran
+  // <div>
+  //   <NetflixSeries />
+  //   <NetflixSeries />
+  //   <NetflixSeries />
+  //   <NetflixSeries />
+  //   <NetflixSeries />
 
-  </div>
+  // </div>
+
+
+  // To avoid from add a extra parent div ,we use fragments in react:
+
+  // there are two way to add fragments, use array and fragment
+
+  // ([ <NetflixSeries key="1" />,  <NetflixSeries key="2" />,  <NetflixSeries key="3" />,  <NetflixSeries key="4" />]);
 };
 
 const NetflixSeries = () => {
