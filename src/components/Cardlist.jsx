3d979132f50290c71b3bd1dd9cb4bf9fd7992da6
Muhammad Rameez <buildwithrameez@gmail.com>
2026-08-08 12:00:03 +0500

    perform destructuring to make code more clean

--- src/components/Cardlist.jsx
+++ src/components/Cardlist.jsx
@@ -1,24 +1,27 @@
 const Cardlist = (props) => {
+  const {name,rating,description,genre,cast,watch_url} = props.currElement;
+  console.log(name);
+  
     return (<li>
       <div>
         <img style={{'marginTop':'20px'}} width='50%' src={props.currElement.img_url} alt="" />
       </div>
     <h2>
-     <b>Movie Name: </b>{props.currElement.name}
+     <b>Movie Name: </b>{name}
     </h2>
     <h3>
-     <b>Rating: </b>{props.currElement.rating}
+     <b>Rating: </b>{rating}
     </h3>
     <p>
-     <b>Summary: </b>{props.currElement.description}
+     <b>Summary: </b>{description}
     </p>
     <p>
-      <b>Genre:</b>{props.currElement.genre}
+      <b>Genre:</b>{genre}
     </p>
     <p>
-       <b>Cost: </b>{props.currElement.cast}
+       <b>Cost: </b>{cast}
     </p>
-    <a href={props.currElement.watch_url} target='_blank'><button>Watch Now</button></a>
+    <a href={watch_url} target='_blank'><button>Watch Now</button></a>
     </li>)
 };
 
