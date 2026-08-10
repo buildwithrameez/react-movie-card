ccbe48ce8c473bbc41d02113e45087e99dbae208
Muhammad Rameez <buildwithrameez@gmail.com>
2026-08-10 14:18:10 +0500

    passing events in react

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import './index.css'
 import { Moviecard }  from './App.jsx'
 import { Practice } from './practices.jsx'
 import Profile from './components/Profile.jsx'
+import { EventHandling } from './components/Events.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Moviecard />
-
+    {/* <Moviecard /> */}
+    <EventHandling />
     {/* <Practice />
 
      challenge file
