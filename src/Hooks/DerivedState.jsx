404bf3ba5afe513eb070e9916f3caf4b2f7d01e6
Muhammad Rameez <buildwithrameez@gmail.com>
2026-08-11 14:33:52 +0500

    learn about derived state

--- src/Hooks/DerivedState.jsx
+++ src/Hooks/DerivedState.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { State } from "./State";
+import '../Hooks/style.css'
 // challenge Test
 
 // method 1 using map
@@ -34,15 +35,23 @@ export const DerivedState2 = () => {
    {name : 'Tuqeer', age : 17}
   ]);
   
-    return <div className="mainDev">
-       <h1>User</h1>
+  const userCount = user.length;
+  const average = (user.reduce((accum,curEle) => accum + curEle.age, 0))/userCount;
+
+    return <div className="mainDiv">
+       <h1>User</h1>)
        <ul>
          {user.map((currElement,index) => {
             const {name,age} = currElement;
-          return <li>
+          return <>
+          <li>
                {name} - {age}
           </li>
-         })};
+          </>
+         })}
+         <p>User Number: {userCount}</p>
+         <p>Aerage age: {average}</p>
+
        </ul>
     </div>
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { Practice } from './practices.jsx'
 import Profile from './components/Profile.jsx'
 import { EventHandling } from './components/Events.jsx'
 import { State } from './Hooks/State.jsx'
-import { DerivedState2 } from './Hooks/DericedState.jsx'
+import { DerivedState2 } from './Hooks/DerivedState.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
